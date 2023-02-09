d44c4ed3f25bdebe86deeddacd0249cee9f57269
mrmongkeyy <mrmongkeyy@gmail.com>
2023-02-09 11:21:43 +0700

    new update

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const route = [
+	{
+		methode:'get',
+		address:'/',
+		callback(req,res){
+			res.sendFile('/more/index.html',{root:path.join(__dirname)},function(err){if(err)console.log(err)});
+		}
+	},
+	{
+		methode:'get',
+		address:'/scripts',
+		callback(req,res){
+			res.sendFile(`/more/${req.query.fn}.js`,{root:path.join(__dirname)},function(err){if(err)console.log(err)});
+		}
+	},
+	{
+		methode:'get',
+		address:'/style',
+		callback(req,res){
+			res.sendFile('/more/style.css',{root:path.join(__dirname)},function(err){if(err)console.log(err)});
+		}
+	}
+];
+route.forEach((opt)=>{app[opt.methode](opt.address,(req,res)=>{opt.callback(req,res)});})
+
+app.listen(process.env.PORT||8080,()=>console.log('am lived!'));
